1eac4cb91416fc8f453fbb0d8582c33dfb944c14
リリ <riri@ririnoMacBook-Pro.local>
2021-02-17 19:38:13 +0900

    groupの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "rooms#index"
+  
   resources :users, only: [:edit, :update]
+  
+  root to: "rooms#index"
+
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
+
   resources :lists, only: [:new, :create]
-  resources :groups, only: [:new, :create]
+
+  resources :groups, only: [:new, :create, :destroy]
 end
